7bcf6fb6432d7b56587e0ad9f41c4c0d663a3a6c
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-01-05 12:30:13 -0500

    completed Poke-Express

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
 const express = require("express")
 const app = express()
+const pokemon = require("./models/pokemon.json")
 
-app.get("/", (req, res) => {
-    res.send("This is the home page")
+// app.get("/", (req, res) => {
+//     res.send("This is the home page")
+// })
+
+// app.get("/bugs", (req, res) => {
+//     // let amtOfBugs = 99
+//     res.send(`<p>
+//             99 little bugs in the code
+//             <br />
+//             <a href="/bugs/195">pull one down, patch it around</a>
+//         </p>`)
+// })
+
+// app.get("/bugs/:numberOfBugs", (req, res) => {
+//     let { numberOfBugs } = req.params
+//     if(numberOfBugs < 200){
+//         res.send(`<p>
+//             ${numberOfBugs} little bugs in the code
+//             <br />
+//             <a href="/bugs/${Number(numberOfBugs) + 2}">pull one down, patch it around</a>
+//         </p>`)
+//     } else {
+//         res.send(`<p>
+//         ${numberOfBugs} little bugs in the code
+//         <br />
+//         <a href="/bugs">Start Over!</a>
+//         </p>`)
+//     }
+// })
+
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
 })
 
-app.get("/bugs", (req, res) => {
-    // let amtOfBugs = 99
-    res.send(`<p>
-            99 little bugs in the code
-            <br />
-            <a href="/bugs/195">pull one down, patch it around</a>
-        </p>`)
+app.get("/pokemon/search", (req, res) => {
+    const { name } = req.query
+    let capLetter = name[0].toUpperCase()
+    let formattedName = capLetter + name.slice(1)
+    let foundPoke = pokemon.find(poke => poke.name === formattedName)
+    
+    if (foundPoke) res.send(foundPoke)
+    else res.send("No Pokemon found!")
 })
 
-app.get("/bugs/:numberOfBugs", (req, res) => {
-    let { numberOfBugs } = req.params
-    if(numberOfBugs < 200){
-        res.send(`<p>
-            ${numberOfBugs} little bugs in the code
-            <br />
-            <a href="/bugs/${Number(numberOfBugs) + 2}">pull one down, patch it around</a>
-        </p>`)
-    } else {
-        res.send(`<p>
-        ${numberOfBugs} little bugs in the code
-        <br />
-        <a href="/bugs">Start Over!</a>
-        </p>`)
-    }
+app.get("/pokemon/:indexOfArray", (req, res) => {
+    const { indexOfArray } = req.params
+    if(pokemon[indexOfArray]) res.send(pokemon[indexOfArray])
+    else res.send(`Sorry, no pokemon found at /pokemon/${indexOfArray}`)
 })
+
 
 module.exports = app
