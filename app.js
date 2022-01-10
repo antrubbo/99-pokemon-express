3112f291ec2a7c64b648ed84576cbb8abc7484c6
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-01-10 15:03:06 -0500

    completed all

--- app.js
+++ app.js
@@ -2,35 +2,37 @@ const express = require("express")
 const app = express()
 const pokemon = require("./models/pokemon.json")
 
-// app.get("/", (req, res) => {
-//     res.send("This is the home page")
-// })
+app.get("/", (req, res) => {
+    res.send("Welcome 99 Pokemon")
+})
 
-// app.get("/bugs", (req, res) => {
-//     // let amtOfBugs = 99
-//     res.send(`<p>
-//             99 little bugs in the code
-//             <br />
-//             <a href="/bugs/195">pull one down, patch it around</a>
-//         </p>`)
-// })
+app.get("/bugs", (req, res) => {
+    res.send(`
+        <p>
+            99 little bugs in the code
+            <br />
+            <a href="/bugs/195">Pull one down, patch it around</a>
+        </p>
+    `)
+})
 
-// app.get("/bugs/:numberOfBugs", (req, res) => {
-//     let { numberOfBugs } = req.params
-//     if(numberOfBugs < 200){
-//         res.send(`<p>
-//             ${numberOfBugs} little bugs in the code
-//             <br />
-//             <a href="/bugs/${Number(numberOfBugs) + 2}">pull one down, patch it around</a>
-//         </p>`)
-//     } else {
-//         res.send(`<p>
-//         ${numberOfBugs} little bugs in the code
-//         <br />
-//         <a href="/bugs">Start Over!</a>
-//         </p>`)
-//     }
-// })
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    let { numberOfBugs } = req.params
+    if(numberOfBugs < 200){
+        res.send(`
+            <p>
+                ${numberOfBugs} little bugs in the code
+                <br />
+                <a href="/bugs/${Number(numberOfBugs) + 2}">Pull one down, patch it around</a>
+            </p>
+        `)
+    } else {
+        res.send(`
+        <p>Too many bugs!! Start over!</p>
+        <a href="/bugs">Start Again</a>
+        `)
+    }
+})
 
 app.get("/pokemon", (req, res) => {
     res.send(pokemon)
@@ -38,18 +40,16 @@ app.get("/pokemon", (req, res) => {
 
 app.get("/pokemon/search", (req, res) => {
     const { name } = req.query
-    let capLetter = name[0].toUpperCase()
-    let formattedName = capLetter + name.slice(1)
-    let foundPoke = pokemon.find(poke => poke.name === formattedName)
+    let foundPoke = pokemon.filter(poke => poke.name.toLowerCase() === name.toLowerCase())
     
-    if (foundPoke) res.send(foundPoke)
-    else res.send("No Pokemon found!")
+    if (foundPoke.length > 0) res.send(foundPoke)
+    else res.send([])
 })
 
 app.get("/pokemon/:indexOfArray", (req, res) => {
     const { indexOfArray } = req.params
     if(pokemon[indexOfArray]) res.send(pokemon[indexOfArray])
-    else res.send(`Sorry, no pokemon found at /pokemon/${indexOfArray}`)
+    else res.send(`Sorry, no pokemon found at ${indexOfArray}`)
 })
 
 
